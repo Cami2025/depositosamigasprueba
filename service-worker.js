301108913c6d01726c08f57366858ae0da66d097
Cami2025 <camiartificial2025@gmail.com>
2025-02-05 08:25:14 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,22 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registrado con éxito:', registration);
-      })
-      .catch(error => {
-        console.log('Error al registrar el Service Worker:', error);
-      });
-  });
-}
+self.addEventListener('install', (event) => {
+  console.log('Service Worker instalado');
+  event.waitUntil(
+    caches.open('camapp-cache-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192x192.png',
+        '/icon-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
